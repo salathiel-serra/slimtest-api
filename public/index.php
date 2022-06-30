d6f9b1aba446a2cbd0f43f3f7db362e8b61c0185
Salathiel Serra <thiel.serra@gmail.com>
2022-06-30 00:43:38 -0300

    Adicionando endpoint para listagem de usuários

--- public/index.php
+++ public/index.php
@@ -9,8 +9,22 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("HELLO WORLD!");
+    $response->getBody()->write("API Running...");
     return $response;
+});
+
+$app->get('/users', function (Request $request, Response $response, $args) {
+    
+    $users = [
+        '1' => 'João Silva',
+        '2' => 'Maria Fernandes',
+        '3' => 'Alice Pereira',
+        '4' => 'Lucas Ximenes',
+        '5' => 'Luis Santos'
+    ];
+
+    $response->getBody()->write( json_encode($users) );
+    return $response->withHeader('Content-type', 'application/json');
 });
 
 $app->run();
